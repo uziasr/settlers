51cf50817f0f705388ed1e66fadf0d407812ce5f
Uzias Rivera <uziasrivera@gmail.com>
2021-01-02 19:31:25 -0800

    improved ui

--- src/components/ActiveTurn.js
+++ src/components/ActiveTurn.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
 import Dice from "./Dice"
 
+let developmentCards = [
+    { type: "Knight", useable: true, img: "image/knight.jpg" },
+    { type: "Victory Point", useable: true, img: "image/victoryPoint.jpg" },
+    { type: "Road Builder", useable: true, img: "image/roadBuilder.jpg" },
+    { type: "Year of Plenty", useable: true, img: "image/yearOfPlenty.jpg" },
+    { type: "Monopoly", useable: true, img: "image/monopoly.jpg" }
+]
+
+let placeHolderCard = { type: "Knight", useable: false, img: "image/knight.jpg" }
+
 
 const ActiveTurn = ({ setBuildType, endTurn, roll, player }) => {
     console.log(player)
     return (
         <div className="userControls">
             <div className="cards">
-               <div className="developmentCardsRoot">
-                   <h3>Development Cards</h3>
-                    <div className="developmentCards">
-                        
-                    </div>
-               </div>
                 <div className="resourceRoot">
                     <h3>Hand</h3>
                     <div className="resources">
@@ -20,7 +24,23 @@ const ActiveTurn = ({ setBuildType, endTurn, roll, player }) => {
                         <div className="resourceCard">🧱 x {player.cards["brick"]}</div>
                         <div className="resourceCard">🌾 x {player.cards["hay"]}</div>
                         <div className="resourceCard">🐑 x {player.cards["sheep"]}</div>
-                        <div className="resourceCard"><span style={{color: "black"}}>⛏</span> x {player.cards["mineral"]}</div>
+                        <div className="resourceCard"><span style={{ color: "black" }}>⛏</span> x {player.cards["mineral"]}</div>
+                    </div>
+                </div>
+                <div className="developmentCardsRoot">
+                    <h3>Development Cards</h3>
+                    <div className="developmentCards">
+                        {developmentCards.length ?
+                            developmentCards.map(dc => (
+                                <div className="developmentCard">
+                                    <img className="developmentCardImg" src={dc.img}></img>
+                                </div>
+                            ))
+                            :
+                            <div style={{ visibility: "hidden" }} className="developmentCard">
+                                <img className="developmentCardImg" alt="placeholder" src={placeHolderCard.img}></img>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
